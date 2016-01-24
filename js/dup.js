b7aea83be34b5ca6fdc58a3c8bc1165fbcb3c313
Shusui MOYATANI <syusui.s@gmail.com>
2016-01-24 13:21:19 +0900

    add button
    easter egg

---
+++ js/dup.js
@@ -0,0 +1,12 @@
+/* duplicate */
+'use strict';
+var duplicate = function(e) {
+	var target = e.target;
+	var clone  = target.cloneNode(true);
+	var rgb = [0,0,0].map(function(e){ return Math.floor(Math.random() * 128) + 128; });
+	clone.style.backgroundColor = 'rgba('+ rgb.join(',') +',1.0)';
+	clone.addEventListener('click', duplicate);
+	target.insertAdjacentElement('afterend', clone);
+};
+var node  = document.querySelector('button.button:nth-child(1)');
+node.addEventListener('click', duplicate);
