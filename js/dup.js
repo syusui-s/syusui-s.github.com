c725b6dc1d8e3c1e0812c6fe5b90b00c5daba764
Shusui MOYATANI <syusui.s@gmail.com>
2016-03-04 19:29:12 +0900

    色合い修正

--- js/dup.js
+++ js/dup.js
@@ -3,7 +3,7 @@
 var duplicate = function(e) {
 	var target = e.target;
 	var clone  = target.cloneNode(true);
-	var rgb = [0,0,0].map(function(e){ return Math.floor(Math.random() * 128) + 128; });
+	var rgb = [0,0,0].map(function(e){ return Math.floor(Math.random() * 0xaf) + 0x50; });
 	clone.style.backgroundColor = 'rgba('+ rgb.join(',') +',1.0)';
 	clone.addEventListener('click', duplicate);
 	target.insertAdjacentElement('afterend', clone);
